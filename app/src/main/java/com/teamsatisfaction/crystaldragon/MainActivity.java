1f75585546ec4e9a5e71bff93630ff61ba30b94e
Adcock <adcockbj@appstate.edu>
2022-04-08 21:53:16 -0400

    updating project with connect and disconnect buttons. No functionality

--- app/src/main/java/com/teamsatisfaction/crystaldragon/MainActivity.java
+++ app/src/main/java/com/teamsatisfaction/crystaldragon/MainActivity.java
@@ -2,12 +2,38 @@ package com.teamsatisfaction.crystaldragon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mConnectButton;
+    private Button mDisconnectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mConnectButton = (Button) findViewById(R.id.connect_button);
+        mConnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        R.string.connecting_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mDisconnectButton = (Button) findViewById(R.id.disconnect_button);
+        mDisconnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        R.string.disconnecting_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
